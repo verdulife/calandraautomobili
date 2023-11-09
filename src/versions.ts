8a917156311bf821a7efa22f6a83886087a82ef7
verdulife <verdu@live.com>
2023-11-09 01:18:30 +0100

    verdu work 4

---
+++ src/versions.ts
@@ -0,0 +1,35 @@
+export const v1 = [
+  "V6 Ecoboost 3.5L",
+  "500HP/5750RPM",
+  "Torque 560Nm",
+  "Twin Turbo direct injection",
+  "Tubular frame",
+  "995 Kg",
+  "Engine position longitudinal",
+  "6 speed manual gearbox",
+  "19” rims",
+];
+
+export const v2 = [
+  "V6 Ecoboost 3.5L",
+  "750HP/6500RPM",
+  "Torque 780Nm",
+  "Twin Turbo direct and port injection",
+  "Tubular frame",
+  "995 Kg",
+  "Engine position: longitudinal",
+  "6 speed manual gearbox",
+  "19” rims",
+];
+
+export const options = [
+  "2 year warranty does not include engine and transmission",
+  "A/C",
+  "Race suspension",
+  "AT NAV + Radio",
+  "Sequential gearbox 6 speed",
+  "FIA engine bay Fire extinguisher",
+  "Carbon fiber seats",
+  "19” Forged rims",
+  "Carbon fiber clutch",
+];
