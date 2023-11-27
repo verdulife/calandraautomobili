2ed99276ececda57181cc7f17b46e8484b4eb98b
verdulife <verdu@live.com>
2023-11-28 00:56:42 +0100

    wtf deno?

--- src/pages/api/form.ts
+++ src/pages/api/form.ts
@@ -2,8 +2,8 @@ import type { APIRoute } from "astro";
 import { SMTPClient } from "emailjs";
 import { intern, response } from "lib/email-template";
 
-const EMAIL = import.meta.env.EMAIL;
-const EMAIL_PASS = import.meta.env.EMAIL_PASS;
+const EMAIL = Deno.env.get("EMAIL");
+const EMAIL_PASS = Deno.env.get("EMAIL_PASS");
 
 const client = new SMTPClient({
   user: EMAIL,
