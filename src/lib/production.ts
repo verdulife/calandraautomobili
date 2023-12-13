e48188fc358952caa44e2413a6fcd1988a174742
verdulife <verdu@live.com>
2023-12-13 21:37:35 +0100

    fix production cards

--- src/lib/production.ts
+++ src/lib/production.ts
@@ -6,12 +6,12 @@ export const cards = [
   },
   {
     title: "Suspension, Geometry and Chassis",
-    desc: "Designed fully in house with the highest detail and refinement lines to be able to achieve the maximum downforce without compromising the styling. Slight changes have been introduced to the GP version. This has been done to improve the overall package. These modifications have arrived to give the driver the most comfort possible both on road and track.",
+    desc: "Also designed in house. The calculations gone into the chassis and suspension have been meticulously revised and tested with the P001. The chassis is a lightweight space frame configuration, to ensure a balanced weight distribution front to rear, optimum stiffness and safety. Each element in the chassis has been positioned to give the perfect balance not only front to rear but also from left to right.",
     icon: "/svg/compass.svg",
   },
   {
     title: "Powertrains",
-    desc: "Designed fully in house with the highest detail and refinement lines to be able to achieve the maximum downforce without compromising the styling. Slight changes have been introduced to the GP version. This has been done to improve the overall package. These modifications have arrived to give the driver the most comfort possible both on road and track.",
+    desc: "Two engine options V6 3.5L Eco boost offering 500Bhp and 750Bhp. The precision and engineering that has gone into the engines, is just remarkable. Hours of machining and calibrating race parts has delivered the reliability needed for the most extreme road and track demands. The cooling system has been carefully studied for minimum heat transfer. 6-speed manual Dog or Sequential gearbox.",
     icon: "/svg/gear.svg",
   },
   {
@@ -21,7 +21,7 @@ export const cards = [
   },
   {
     title: "Interior",
-    desc: "Simplistic design with high quality finishing. Detail is extraordinary for both road and track versions where we have maintained the simplicity for the driver to only focus on the road. All aluminum buttons and knobs are CNC machined. Trimmed cockpit and surroundings in Alcantara. Race seats and a 4-point harness belt, offers firm occupant support giving you that GT race car feeling.",
+    desc: "The choice of 4 or 6 piston caliper is available depending on demands. These have been studied to be able to offer the customer various versions depending on their use. Piston size has been crucial to determinate the best option for either road or track use. With standard monoblock aluminium 19” rims, we also offer a forged version for the demanding drivers.",
     icon: "/svg/glass.svg",
   },
   {
