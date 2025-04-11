25418aca4c6107b887e5e966aea92cc988e2e185
verdulife <verdu@live.com>
2025-04-11 21:12:26 +0200

    full change of production

--- src/lib/production.ts
+++ src/lib/production.ts
@@ -1,32 +1,32 @@
 export const cards = [
   {
     title: "Aerodynamics and Design",
-    desc: "Designed fully in house with the highest detail and refinement lines to be able to achieve the maximum downforce without compromising the styling. Slight changes have been introduced to the GP version. This has been done to improve the overall package. These modifications have arrived to give the driver the most comfort possible both on road and track.",
+    desc: "Designed fully in house with the highest detail and refinement. Achieving the maximum downforce without compromising design with aerodynamical elements, which has been done to improve the overall package. These modifications have arrived for the driver to feel the need of aggressiveness, downforce focusing and high speed track behavior.",
     icon: "/svg/wind.svg",
   },
   {
     title: "Suspension, Geometry and Chassis",
-    desc: "Also designed in house. The calculations gone into the chassis and suspension have been meticulously revised and tested with the P001. The chassis is a lightweight space frame configuration, to ensure a balanced weight distribution front to rear, optimum stiffness and safety. Each element in the chassis has been positioned to give the perfect balance not only front to rear but also from left to right.",
+    desc: "The calculations gone into the chassis and suspension have been meticulously revised and tested on the new space frame developed for the GPS and GPC which share the same double wishbone, monotube dampers and space framed chassis. The suspension geometry has been a key factor for high-speed cornering and, together with the F45%-R55% and a 50% L-R weight distribution, has made the GPSeries a brutal track car. A lightweight space frame configuration, to ensure an optimum stiffness and safety where each element in the chassis, has been positioned to give the perfect balance.",
     icon: "/svg/compass.svg",
   },
   {
     title: "Powertrains",
-    desc: "Two engine options V6 3.5L Eco boost offering 500Bhp and 750Bhp. The precision and engineering that has gone into the engines, is just remarkable. Hours of machining and calibrating race parts has delivered the reliability needed for the most extreme road and track demands. The cooling system has been carefully studied for minimum heat transfer. 6-speed manual Dog or Sequential gearbox.",
+    desc: "Two engine options by Ford, the V6 3.5L Eco boost, offering 500Bhp and 750Bhp. The precision and engineering that has gone into the GPCorsa with it’s 750Bhp engine, is just remarkable. All engine internals have been reengineered to gain the power and torque needed for the demanding needs of our customers. Hours of machining and calibrating race parts has delivered the reliability needed for the most extreme road and track demands. The GPSport and it’s 500Bhp engine, has been revised to give the perfect performance on road, making it also a daily driver for the motorsport enthusiasts. The cooling system has been carefully studied for minimum heat transfer, including a double intercooler package and with single radiator. A 6-speed manual Dog or Sequential gearbox are both available in for both versions.",
     icon: "/svg/gear.svg",
   },
   {
     title: "Brakes and Wheels",
-    desc: "The choice of 4 or 6 piston caliper is available depending on demands. These have been studied to be able to offer the customer various versions depending on their use. Piston size has been crucial to determinate the best option for either road or track use. With standard monoblock aluminium 19” rims, we also offer a forged version for the demanding drivers.",
+    desc: "The choice of a 4 or 6 piston caliper is available depending on customer demands. These have been studied to be able to offer various versions depending on their use. Piston size has been crucial to determinate the best option for either road or track use. With standard monoblock aluminum 19” rims, we also offer a forged version for demanding drivers.",
     icon: "/svg/tire.svg",
   },
   {
     title: "Interior",
-    desc: "Simplistic design with high quality finishing. Detail is extraordinary for both road and track versions where we have maintained the simplicity for the driver to only focus on the road. All aluminum buttons and knobs are CNC machined. Trimmed cockpit and surroundings in Alcantara. Race seats and a 4-point harness belt, offers firm occupant support giving you that GT race car feeling.",
+    desc: "Designed with a track focused vision that includes, high quality finishing and detailing in alcantara and aluminum. All buttons and knobs are CNC machined from aluminum. A Trimmed cockpit with surroundings in alcantara. Race seats and a 4-point harness belt, offers firm occupant support, giving you a GT race car feeling. The seating and pedal position, is designed specifically for each customer. The driver position is extremely important and that’s why at Calandra, it has all been engineered meticulously, thinking in weight distribution and comfort ad the same time.",
     icon: "/svg/glass.svg",
   },
   {
-    title: "The Veredict",
-    desc: "The only inspired GT series race car that has evolved into a sports car. After the road and track tests performed till today, the feeling is brutal, but we continue our testing to be able to offer our customers the best handling sports car without electronic management for driver assistance. Our inspiration and philosophy is to continue building true sports car, were the driver and car are united to the road.",
+    title: "The Verdict",
+    desc: "The only inspired GT series race car that has evolved into a sports car. After the road and track tests performed till today, the feeling is brutal, but we continue our testings to be able to offer our customers the best handling sports car without electronic management or driver assistance. Our inspiration and philosophy is to continue building a true sports car, where the driver and car are united with each other on road and track.",
     icon: "/svg/hammer.svg",
   },
 ];
